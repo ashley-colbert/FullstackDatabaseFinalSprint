2c7580dbf0b22211081b2b901662da648d80c894
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-03-30 19:16:34 -0230

    routes edited

---
+++ routes/petType.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const ordersDal = require('../services/orders.dal')
+
+// https://localhost:3000/petType/
+router.get('/', async (req, res) => {
+    try {
+        let thePetType = await petTypeDal.getPetType();
+        if(DEBUG) console.table(thePetType);
+        res.render('petType', {thePetType});
+    } catch {
+        res.render('503');
+    }
+});
+
+// api/petType/:id - will return one specific entry that matched a particular type_id from the database
+router.get('/:id', async (req, res) => {
+    try {
+        const aPetType = await petTypeDal.getPetTypeById(req.params.type_id);
+        if(DEBUG) console.log(`petType.router.get/:id ${aPetType}`);
+        if (aPetType)
+            res.render('petType', {aPetType});
+        else
+            res.render('norecord');
+    } catch {
+        res.render('503');
+    }
+});
+
+// this router will replace an entry in the database that matches the type_id
+router.get('/:id/replace', async (req, res) => {
+    if(DEBUG) console.log('petType.Replace : ' + req.params.type_id);
+    res.render('petTypePut.ejs', { type_id: req.params.type_id, name: req.query.name, life_expectancy: req.query.life_expectancy, scientific_name: req.query.scientific_name});
+});
+
+//this will edit an entry in the database using the type_id
+router.get('/:id/edit', async (req, res) => {
+    if(DEBUG) console.log('petType.Edit : ' + req.params.type_id);
+    res.render('petTypePatch.ejs', { type_id: req.params.type_id, name: req.query.name, life_expectancy: req.query.life_expectancy, scientific_name: req.query.scientific_name});
+});
+
+//this will delete an entry from the database using the type_id
+router.get('/:id/delete', async (req, res) => {
+    if(DEBUG) console.log('petType.Delete : ' + req.params.type_id);
+    res.render('petTypeDelete.ejs', {type_id: req.params.type_id, name: req.query.name, life_expectancy: req.query.life_expectancy, scientific_name: req.query.scientific_name});
+});
+
+//router to post a new entry into the database
+router.post('/', async (req, res) => {
+    if(DEBUG) console.log("petType.POST");
+    try {
+        await petTypeDal.addPetType(req.body.type_id, req.query.name, req.query.life_expectancy, req.query.scientific_name);
+        res.redirect('/petType/');
+    } catch {
+        res.render('503');
+    }
+});
+
+// PUT, PATCH, and DELETE are part of HTTP, not a part of HTML
+
+router.put('/:id', async (req, res) => {
+    if(DEBUG) console.log('petType.PUT: ' + req.params.type_id);
+    try {
+        await petTypeDal.putPetType(req.params.type_id, req.query.name, req.query.life_expectancy, req.query.scientific_name );
+        res.redirect('/petType/');
+    } catch {
+        res.render('503');
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    if(DEBUG) console.log('petType.PATCH: ' + req.params.type_id);
+    try {
+        await petTypeDal.patchPetType(req.params.type_id, req.query.name, req.query.life_expectancy, req.query.scientific_name  );
+        res.redirect('/petType/');
+    } catch {
+        res.render('503');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    if(DEBUG) console.log('petType.DELETE: ' + req.params.type_id);
+    try {
+        await petTypeDal.deletePetType(req.params.type_id);
+        res.redirect('/petType/');
+    } catch (err) {
+        if(DEBUG) console.error(err);
+        res.render('503');
+    }
+});
+
+module.exports = router
---
+++ routes/pets.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const ordersDal = require('../services/orders.dal')
+
+// https://localhost:3000/pets/
+router.get('/', async (req, res) => {
+    try {
+        let thePets = await petsDal.getPets();
+        if(DEBUG) console.table(thePets);
+        res.render('pets', {thePets});
+    } catch {
+        res.render('503');
+    }
+});
+
+// api/pets/:id - will return one specific entry that matched a particular pet_id from the database
+router.get('/:id', async (req, res) => {
+    try {
+        const aPet = await petsDal.getPetsById(req.params.pet_id);
+        if(DEBUG) console.log(`pets.router.get/:id ${aPet}`);
+        if (aPet)
+            res.render('pets', {aPet});
+        else
+            res.render('norecord');
+    } catch {
+        res.render('503');
+    }
+});
+
+// this router will replace an entry in the database that matches the pet_id
+router.get('/:id/replace', async (req, res) => {
+    if(DEBUG) console.log('pets.Replace : ' + req.params.pet_id);
+    res.render('petsPut.ejs', { pet_id: req.params.pet_id, pet_name: req.query.pet_name, age: req.query.age, type_Id: req.query.type_id, owner_id: req.query.owner_id});
+});
+
+//this will edit an entry in the database using the pet_id
+router.get('/:id/edit', async (req, res) => {
+    if(DEBUG) console.log('pets.Edit : ' + req.params.pet_id);
+    res.render('petsPatch.ejs', { pet_id: req.params.pet_id, pet_name: req.query.pet_name, age: req.query.age, type_Id: req.query.type_id, owner_id: req.query.owner_id});
+});
+
+//this will delete an entry from the database using the pet_id
+router.get('/:id/delete', async (req, res) => {
+    if(DEBUG) console.log('pets.Delete : ' + req.params.pet_id);
+    res.render('petsDelete.ejs', {pet_id: req.params.pet_id, pet_name: req.query.pet_name, age: req.query.age, type_Id: req.query.type_id, owner_id: req.query.owner_id});
+});
+
+//router to post a new entry into the database
+router.post('/', async (req, res) => {
+    if(DEBUG) console.log("pets.POST");
+    try {
+        await petsDal.addPet(req.body.pet_id, req.query.pet_name, req.query.age, req.query.type_id, req.query.owner_id);
+        res.redirect('/pets/');
+    } catch {
+        res.render('503');
+    }
+});
+
+// PUT, PATCH, and DELETE are part of HTTP, not a part of HTML
+
+router.put('/:id', async (req, res) => {
+    if(DEBUG) console.log('pets.PUT: ' + req.params.pet_id);
+    try {
+        await petsDal.putPet(req.params.pet_id, req.query.pet_name, req.query.age, req.query.type_id, req.query.owner_id );
+        res.redirect('/pets/');
+    } catch {
+        res.render('503');
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    if(DEBUG) console.log('pets.PATCH: ' + req.params.pet_id);
+    try {
+        await petsDal.patchPets(req.params.pet_id, req.query.pet_name, req.query.age, req.query.type_id, req.query.owner_id );
+        res.redirect('/pets/');
+    } catch {
+        res.render('503');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    if(DEBUG) console.log('pets.DELETE: ' + req.params.pet_id);
+    try {
+        await petsDal.deletePet(req.params.pet_id);
+        res.redirect('/pets/');
+    } catch (err) {
+        if(DEBUG) console.error(err);
+        res.render('503');
+    }
+});
+
+module.exports = router
